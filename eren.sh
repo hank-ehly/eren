207142ef8793e489b2524a206fcff3d28c90151a
Hank Ehly <henry.ehly@gmail.com>
2016-07-03 11:49:29 +0900

    Allow any ordering of flag options

--- eren.sh
+++ eren.sh
@@ -3,6 +3,7 @@
 SHUNIT2_DL='https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz'
 SHUNIT2_TB='shunit2-2.1.6.tgz'
 VERBOSE='no'
+RUN_TESTS='no'
 
 usage() {
     echo '
@@ -66,6 +67,38 @@ log() {
     clog black "${1}"
 }
 
+download_shunit2() {
+    debug 'mkdir vendor && cd vendor'
+    mkdir vendor && cd vendor
+
+    debug "wget ${SHUNIT2_DL}"
+    wget ${SHUNIT2_DL} > /dev/null 2>&1
+
+    debug "test ! -f ${SHUNIT2_TB}"
+    if [[ ! -f ${SHUNIT2_TB} ]]; then
+        error 'Failed to fetch tarball. Exit.'
+    fi
+
+    debug "tar xzf ${SHUNIT2_TB}"
+    tar xzf ${SHUNIT2_TB}
+
+    debug "rm -f ${SHUNIT2_TB}"
+    rm -f ${SHUNIT2_TB}
+
+    debug "test -f ./shunit2-2.1.6/src/shunit2"
+    if [[ -f ./shunit2-2.1.6/src/shunit2 ]]; then
+        log 'Installation successful.'
+
+        debug "cd .."
+        cd ..
+
+        debug "sh ./eren.sh -t"
+        sh ./eren.sh -t
+    else
+        error 'Installation failed. Exit.'
+    fi
+}
+
 run_shunit2() {
 
     debug "test ! -f ./vendor/shunit2-2.1.6/src/shunit2"
@@ -77,38 +110,7 @@ run_shunit2() {
 
         case ${serialized_reply} in
             y)
-                debug "Downloading shunit2 from ${SHUNIT2_DL}"
-                (
-                    debug 'mkdir vendor && cd vendor'
-                    mkdir vendor && cd vendor
-
-                    debug "wget ${SHUNIT2_DL}"
-                    wget ${SHUNIT2_DL} > /dev/null 2>&1
-
-                    debug "test ! -f ${SHUNIT2_TB}"
-                    if [[ ! -f ${SHUNIT2_TB} ]]; then
-                        error 'Failed to fetch tarball. Exit.'
-                    fi
-
-                    debug "tar xzf ${SHUNIT2_TB}"
-                    tar xzf ${SHUNIT2_TB}
-
-                    debug "rm -f ${SHUNIT2_TB}"
-                    rm -f ${SHUNIT2_TB}
-
-                    debug "test -f ./shunit2-2.1.6/src/shunit2"
-                    if [[ -f ./shunit2-2.1.6/src/shunit2 ]]; then
-                        log 'Installation successful.'
-
-                        debug "cd .."
-                        cd ..
-
-                        debug "sh ./eren.sh -t"
-                        sh ./eren.sh -t
-                    else
-                        error 'Installation failed. Exit.'
-                    fi
-                )
+                download_shunit2
                 ;;
             n)
                 log 'Exit.' && exit 0
@@ -135,7 +137,7 @@ while getopts 'htrv' opt; do
             usage
             ;;
         t)
-            run_shunit2
+            RUN_TESTS='yes'
             ;;
         r)
             echo 'recursive'
@@ -148,3 +150,7 @@ while getopts 'htrv' opt; do
             ;;
     esac
 done
+
+if [[ ${RUN_TESTS} = 'yes' ]]; then
+    run_shunit2
+fi
